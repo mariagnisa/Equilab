6799a5625a0f25d4f2f27cef279c931a6175c3c0
Maria <maria.gnisa@gmail.com>
2017-09-21 14:02:48 +0200

    added img and changed structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../style/App.min.css';
+import '../assets/style/App.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>Nu lär vi oss React! </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
+import './assets/style/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
