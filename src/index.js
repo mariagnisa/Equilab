d013e5a3b501be70f7305a54566637ced1448bdf
Kristjan Frederiksen <kristjanf@gmail.com>
2017-09-22 17:44:25 +0200

    background image

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //import moduls
+import Background from './components/Background';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Start from './components/Start';
 import Hiring from './components/Hiring';
-import Faq from './components/Faq';
+import Faq from './components/FAQ';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>
+    <div style={Background}>
       <Header />
       <Switch>
         <Route path="/faq" component={Faq} />
