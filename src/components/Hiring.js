311cd70ff52443170a61bd2aba3a492e0e3fd976
Maria <maria.gnisa@gmail.com>
2017-09-23 15:54:49 +0200

    working on hiring

--- src/components/Hiring.js
+++ src/components/Hiring.js
@@ -4,9 +4,20 @@ import '../style/Hiring.css';
 
 class Hiring extends Component {
   render() {
+    let headline = "We're growing! And we want to meet awesome new people.";
+    let para = "If you don't feel like reading, just come by for a coffee instead, we are located at Holtermansgatan 1c, Gothenburg.";
+    let pos = "Available Positions:";
+
     return (
-      <div>
-        <p>Hiring page </p>
+      <div className="container">
+        <div>
+          <h4>{ headline }</h4>
+          <p>{ para }</p>
+        </div>
+
+        <div>
+          <h4>{pos}</h4>
+        </div>
       </div>
     );
   }
