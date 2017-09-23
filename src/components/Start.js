e17cab0288f488a74284eb16a895347449270aec
Kristjan Frederiksen <kristjanf@gmail.com>
2017-09-23 17:41:45 +0200

    Started on homepage

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import phoneAnimation from '../assets/img/phone_slids.gif';
 //import Routes from './Routes';
 
 import '../style/Start.css';
 
 class Start extends Component {
   render() {
+    const headline = 'Details';
+    const para = 'Afterwards you can analyze your training by looking at your performance, energy consumption, gait distrubution, intensity jumps, intensity,stride, beat, pace, distance, speed,surface hardness, nr of jumps, etc....';
+
     return (
       <div className="container">
-        <p>homepage </p>
+        <h3>{ headline }</h3>
+        <p>{ para }</p>
+        <img src={phoneAnimation} alt=""/>
       </div>
     );
   }
