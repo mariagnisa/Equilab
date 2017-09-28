094d72fa0e37edb658cd1cad499a901836e8d299
Kristjan Frederiksen <kristjanf@gmail.com>
2017-09-28 15:21:14 +0200

    minor responsive slider fix

--- src/components/Start.js
+++ src/components/Start.js
@@ -8,13 +8,13 @@ import '../style/Start.css';
 class Start extends Component {
   render() {
     return (
-      <div className="container d-flex flex-column">
+      <div className="container d-flex flex-column flex-md-row">
         <div className="col-md-8">
           <Slogan />
         </div>
-        {/* <div className="col-md-4">
+        <div className="col-md-4">
           <Slider />
-        </div> */}
+        </div>
       </div>
     );
   }
