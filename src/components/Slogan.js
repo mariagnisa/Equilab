c5846be3ad6afe2591d2c2a8148b7fed75ddbae4
Kristjan Frederiksen <kristjanf@gmail.com>
2017-10-01 12:42:46 +0200

    fix slogan in mobile view

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -5,7 +5,7 @@ import '../style/Slogan.css';
 class Slogan extends Component {
   render() {
     return (
-      <div className="container slogan">
+      <div className="slogan container">
         <h1><strong>EQUILAB</strong></h1>
         <h2>MORE RIDING</h2>
         <h2 className="lessWorry">LESS WORRY</h2>
